1b13b6fed904b4b519ac11660c1c3b05fbc1b2c9
dumboj <597824751@qq.com>
2021-10-19 23:21:36 +0800

    fix ignore file can not filter mvn etc files

---
+++ src/main/java/cn/dumboj/demo/SpringCloudLearingApplication.java
@@ -0,0 +1,13 @@
+package cn.dumboj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLearingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLearingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/dumboj/demo/SpringCloudLearingApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dumboj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLearingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
